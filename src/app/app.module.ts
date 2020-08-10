7ecbd75ccfe63ef2be44da28564334c1b8069106
captaintaheer <captaintaheer@gmail.com>
2020-08-10 17:56:49 +0800

    installed firbase for backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,20 @@ import { MatVideoModule } from 'mat-video';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatIconModule } from '@angular/material/icon';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: 'AIzaSyCyCcR5kpj9vIhO2zoX4svBSe6NFVYrusg',
+  authDomain: 'vanna-test-45cc3.firebaseapp.com',
+  databaseURL: 'https://vanna-test-45cc3.firebaseio.com',
+  projectId: 'vanna-test-45cc3',
+  storageBucket: 'vanna-test-45cc3.appspot.com',
+  messagingSenderId: '1031929526327',
+};
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -20,6 +34,10 @@ import { MatIconModule } from '@angular/material/icon';
     MatVideoModule,
     NgbModule,
     MatIconModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
